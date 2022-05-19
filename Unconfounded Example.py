2785fccdf110ab371c2ed4e1e379dd6ac1729740
Albe <albuz.93@gmail.com>
2022-05-19 18:49:58 +0100

    #1

--- Unconfounded Example.py
+++ Unconfounded Example.py
@@ -13,9 +13,10 @@ from econml.dml import CausalForestDML
 import warnings
 warnings.filterwarnings("ignore")
 
-
-RMSE_mod = {'S-DNN': [], 'T-DNN': [], 'Causal Forest': [], 'R-DNN': [],
-            'R-sep-DNN': [], 'R-NAM': [], 'R-sep-NAM': [], 'R-sep-mix': []}
+RMSE_mod_Train = {'S-DNN': [], 'T-DNN': [], 'Causal Forest': [], 'R-DNN': [],
+                  'R-sep-DNN': [], 'R-NAM': [], 'R-sep-NAM': [], 'R-sep-mix': []}
+RMSE_mod_Test = {'S-DNN': [], 'T-DNN': [], 'Causal Forest': [], 'R-DNN': [],
+                 'R-sep-DNN': [], 'R-NAM': [], 'R-sep-NAM': [], 'R-sep-mix': []}
 
 N, P, B = 2000, 10, 100
 
@@ -39,14 +40,6 @@ for b in range(B):
     Y_train, Y_test = tr_te_split(Y, split)
 
     ITE_train, ITE_test = tr_te_split(ITE, split)
-
-
-    # Estimate p(A | X)
-    mod_A = LogisticRegression(solver='lbfgs', max_iter=1000)
-    mod_A.fit(X=X_train, y=A_train)
-
-    pred_A = mod_A.predict_proba(X_test)[:, 1]
-
 
     # Models unpacking
     mod_RDNN = R_DNN(D_in=X.shape[1], D_hid=[20, 20], D_trt=2)
@@ -77,14 +70,15 @@ for b in range(B):
 
 
     ###### S-NN
-    test_size = A_test.shape[0]
+    size = A.shape[0]
     mod_S = MLPRegressor(hidden_layer_sizes=(20, 20), solver='adam', max_iter=2000)
     mod_S.fit(X=np.c_[X_train, A_train], y=Y_train)
 
-    pred_1 = mod_S.predict(X=np.c_[X_test, np.ones(test_size)])
-    pred_0 = mod_S.predict(X=np.c_[X_test, np.zeros(test_size)])
+    pred_1 = mod_S.predict(X=np.c_[X, np.ones(size)])
+    pred_0 = mod_S.predict(X=np.c_[X, np.zeros(size)])
 
-    RMSE_mod['S-DNN'].append(RMSE(ITE_test, pred_1 - pred_0))
+    RMSE_mod_Train['S-DNN'].append(RMSE(ITE_train, (pred_1 - pred_0)[split]))
+    RMSE_mod_Test['S-DNN'].append(RMSE(ITE_test, (pred_1 - pred_0)[~split]))
 
 
     ###### T-NN
@@ -97,19 +91,21 @@ for b in range(B):
     mod_dict['mod_T1'].fit(X=X_train[A_train == 1, :], y=Y_train.reshape(-1)[A_train == 1])
     mod_dict['mod_T0'].fit(X=X_train[A_train == 0, :], y=Y_train.reshape(-1)[A_train == 0])
 
-    pred_1 = mod_dict['mod_T1'].predict(X=X_test)
-    pred_0 = mod_dict['mod_T0'].predict(X=X_test)
+    pred_1 = mod_dict['mod_T1'].predict(X=X)
+    pred_0 = mod_dict['mod_T0'].predict(X=X)
 
-    RMSE_mod['T-DNN'].append(RMSE(ITE_test, pred_1 - pred_0))
+    RMSE_mod_Train['T-DNN'].append(RMSE(ITE_train, (pred_1 - pred_0)[split]))
+    RMSE_mod_Test['T-DNN'].append(RMSE(ITE_test, (pred_1 - pred_0)[~split]))
 
 
     ###### R-Causal Forest
     causal_forest = CausalForestDML()
     causal_forest.fit(Y=np.array(Y_train).astype(float), T=np.array(A_train).astype(float),
                       X=np.array(X_train).astype(float), W=None)
-    pred_CF = causal_forest.effect(X=X_test)
+    pred_CF = causal_forest.effect(X=X)
 
-    RMSE_mod['Causal Forest'].append(RMSE(ITE_test, pred_CF))
+    RMSE_mod_Train['Causal Forest'].append(RMSE(ITE_train, pred_CF[split]))
+    RMSE_mod_Test['Causal Forest'].append(RMSE(ITE_test, pred_CF[~split]))
 
 
     ###### R_DNN
@@ -129,10 +125,10 @@ for b in range(B):
     with torch.no_grad():
 
         mod_RDNN.eval()
-        pred_ITE_A_RDNN = mod_RDNN(X_test)[:, 1].reshape(-1)
+        pred_ITE_A_RDNN = mod_RDNN(X)[:, 1].reshape(-1)
 
-    RMSE_mod['R-DNN'].append(RMSE(ITE_test, pred_ITE_A_RDNN))
-
+    RMSE_mod_Train['R-DNN'].append(RMSE(ITE_train, pred_ITE_A_RDNN[split]))
+    RMSE_mod_Test['R-DNN'].append(RMSE(ITE_test, pred_ITE_A_RDNN[~split]))
 
 
     ###### R-sep-DNN
@@ -152,9 +148,10 @@ for b in range(B):
     with torch.no_grad():
 
         mod_sep_RDNN.eval()
-        _, pred_ITE_A_sep_RDNN = mod_sep_RDNN(X_test)
+        _, pred_ITE_A_sep_RDNN = mod_sep_RDNN(X)
 
-    RMSE_mod['R-sep-DNN'].append(RMSE(ITE_test, pred_ITE_A_sep_RDNN))
+    RMSE_mod_Train['R-sep-DNN'].append(RMSE(ITE_train, pred_ITE_A_sep_RDNN[split]))
+    RMSE_mod_Test['R-sep-DNN'].append(RMSE(ITE_test, pred_ITE_A_sep_RDNN[~split]))
 
 
 
@@ -174,10 +171,10 @@ for b in range(B):
     with torch.no_grad():
 
         mod_RNAM.eval()
-        pred_ITE_A_RNAM = mod_RNAM(X_test)[:, 1].reshape(-1)
+        pred_ITE_A_RNAM = mod_RNAM(X)[:, 1].reshape(-1)
 
-    RMSE_mod['R-NAM'].append(RMSE(ITE_test, pred_ITE_A_RNAM))
-
+    RMSE_mod_Train['R-NAM'].append(RMSE(ITE_train, pred_ITE_A_sep_RDNN[split]))
+    RMSE_mod_Test['R-NAM'].append(RMSE(ITE_test, pred_ITE_A_sep_RDNN[~split]))
 
 
     ###### R-sep-NAM
@@ -197,9 +194,10 @@ for b in range(B):
     with torch.no_grad():
 
         mod_sep_RNAM.eval()
-        _, pred_ITE_A_sep_RNAM = mod_sep_RNAM(X_test)
+        _, pred_ITE_A_sep_RNAM = mod_sep_RNAM(X)
 
-    RMSE_mod['R-sep-NAM'].append(RMSE(ITE_test, pred_ITE_A_sep_RNAM))
+    RMSE_mod_Train['R-sep-NAM'].append(RMSE(ITE_train, pred_ITE_A_sep_RNAM[split]))
+    RMSE_mod_Test['R-sep-NAM'].append(RMSE(ITE_test, pred_ITE_A_sep_RNAM[~split]))
 
 
     ###### R-sep-mix
@@ -219,23 +217,36 @@ for b in range(B):
     with torch.no_grad():
 
         mod_Rmix.eval()
-        _, pred_ITE_A_Rmix = mod_Rmix(X_test)
+        _, pred_ITE_A_Rmix = mod_Rmix(X)
 
-    RMSE_mod['R-sep-mix'].append(RMSE(ITE_test, pred_ITE_A_Rmix))
+    RMSE_mod_Train['R-sep-mix'].append(RMSE(ITE_train, pred_ITE_A_Rmix[split]))
+    RMSE_mod_Test['R-sep-mix'].append(RMSE(ITE_test, pred_ITE_A_Rmix[~split]))
 
 
 
-print('\n\nCATE:\n')
-for i, j in RMSE_mod.items():
+
+print('\n\nTrain CATE:\n')
+for i, j in RMSE_mod_Train.items():
+    print('%s - RMSE: %s, SE: %s' % (i, np.round(torch.mean(torch.Tensor(j)).item(), 4),
+                                     np.round(MC_se(torch.Tensor(j), B).item(), 4)))
+
+print('\n\nTest CATE:\n')
+for i, j in RMSE_mod_Test.items():
     print('%s - RMSE: %s, SE: %s' % (i, np.round(torch.mean(torch.Tensor(j)).item(), 4),
                                      np.round(MC_se(torch.Tensor(j), B).item(), 4)))
 
 
 with open('./Results.txt', 'w') as f:
-    f.write('CATE:\n')
-    for i, j in RMSE_mod.items():
+    f.write('Train CATE:\n')
+    for i, j in RMSE_mod_Train.items():
         f.write('\n%s - RMSE: %s, SE: %s' % (i, np.round(torch.mean(torch.Tensor(j)).item(), 4),
                                              np.round(MC_se(torch.Tensor(j), B).item(), 4)))
+
+    f.write('\n\nTest CATE:\n')
+    for i, j in RMSE_mod_Test.items():
+        f.write('\n%s - RMSE: %s, SE: %s' % (i, np.round(torch.mean(torch.Tensor(j)).item(), 4),
+                                             np.round(MC_se(torch.Tensor(j), B).item(), 4)))
+
 
 # R-sep-NAM visualizer on tau_A and tau_M for interpretability
 import matplotlib.pyplot as plt
